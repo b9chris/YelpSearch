d4c14bbca5bf0eac56b8522bc08ff355f1cb9401
Chris Moschini <chris@brass9.com>
2013-05-23 18:55:05 -0700

    v0.1

---
+++ yelpsearch.js
@@ -0,0 +1,16 @@
+// A generic onclick callback function.
+function yelpSearch(info, tab) {
+	chrome.tabs.create({
+		url: 'http://www.yelp.com/search?find_desc=' + encodeURIComponent(info.selectionText)
+	});
+}
+
+(function() {
+	var id = chrome.contextMenus.create({
+		title: 'Search Yelp for "%s"',
+		contexts: ['selection'],
+		onclick: yelpSearch
+	});
+})();
+
+
